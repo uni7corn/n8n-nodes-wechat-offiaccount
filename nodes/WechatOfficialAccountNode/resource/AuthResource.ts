e377a505a5e51c3a587314904ea7602f84f5118f
imblowsnow <im.blowsnow@gmail.com>
2025-03-29 20:51:35 +0800

    feat: 获取accesstoken feat: 新增上传永久其他素材

---
+++ nodes/WechatOfficialAccountNode/resource/AuthResource.ts
@@ -0,0 +1,6 @@
+import { INodePropertyOptions } from 'n8n-workflow/dist/Interfaces';
+
+export default {
+	name: '授权',
+	value: 'auth',
+} as INodePropertyOptions;
---
+++ nodes/WechatOfficialAccountNode/resource/auth/GetAccessTokenOperate.ts
@@ -0,0 +1,22 @@
+import { IDataObject, IExecuteFunctions } from 'n8n-workflow';
+import { ResourceOperations } from '../../../help/type/IResource';
+import RequestUtils from '../../../help/utils/RequestUtils';
+
+export default {
+	name: '获取',
+	value: 'auth:getAccessToken',
+	options: [],
+	async call(this: IExecuteFunctions, index: number): Promise<IDataObject> {
+		// 调用一次验证/刷新 access_token
+		await RequestUtils.request.call(this, {
+			method: 'GET',
+			url: `/cgi-bin/get_api_domain_ip`,
+		});
+
+		const credentials = await this.getCredentials('wechatOfficialAccountCredentialsApi');
+
+		return {
+			accessToken: credentials.accessToken,
+		};
+	},
+} as ResourceOperations;
